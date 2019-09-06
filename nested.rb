e341f8bec8b76bbf6fb9b79d11ef360ab2ad6804
Arya <arya721@gmail.com>
2019-09-06 22:57:19 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,8 +60,6 @@ def dennis_ritchies_language
 programmer_hash[:dennis_ritchie][:languages][0]
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-
 
 	programmer_hash =
  		{
