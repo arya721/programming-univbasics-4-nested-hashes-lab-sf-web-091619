08821381dcdd7bf41a20f40bb41c3b149221e939
Arya <arya721@gmail.com>
2019-09-06 22:57:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,13 +60,14 @@ def dennis_ritchies_language
 programmer_hash[:dennis_ritchie][:languages][0]
 
 def adding_matz
+# add the following information to the top level of programmer_hash
+# :yukihiro_matsumoto => {
+#   :known_for => "Ruby",
+#    :languages => ["LISP", "C"]
+# }
 
 	programmer_hash =
  		{
-			   :yukihiro_matsumoto => {
-			   :known_for => "Ruby",
-			   :languages => ["LISP", "C"]
-			 }
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -80,6 +81,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
+
 
 end
 
